50253526e9e4ab977bd13b06cf74c529f4378520
github username <github email address>
2019-04-15 17:46:48 -0400

    making progress on lab

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- INSERT INTO bears (name, age, gender ) VALUES ('Mr. chocolate', 4, 'male');
+-- INSERT INTO bears (name, age, gender) VALUES ('Rowdy', 6, 'male');
+-- INSERT INTO bears (name, age, gender) VALUES ('Tabitha', 5, 'female');
+-- INSERT INTO bears (name, age, gender) VALUES ('Sergeant Brown', 9, 'male');
+-- INSERT INTO bears (name, age, gender) VALUES ('Melissa', 10, 'female');
+-- INSERT INTO bears (name, age, gender) VALUES ('Grinch', 2, 'male');
+-- INSERT INTO bears (name, age, gender) VALUES ('Wendy', 6, 'female');
+-- INSERT INTO bears (name, age, gender) VALUES (NULL, 11, NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
